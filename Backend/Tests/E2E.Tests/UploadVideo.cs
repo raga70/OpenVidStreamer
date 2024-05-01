edd736ed41ebafcbad5582a2566a91e84950dc2f
Radoslav Radev <radoslav@radev.software>
2024-05-01 21:58:12 +0200

    added local copy for videoUpload E2E test in CI pipe so its in the repo

--- Backend/Tests/E2E.Tests/UploadVideo.cs
+++ Backend/Tests/E2E.Tests/UploadVideo.cs
@@ -17,13 +17,16 @@ public class UploadVideo
     [Fact]
     public void Upload()
     {
+        var repoRoot = GetRepositoryRoot();
+        var videoFilePath = Path.Combine(repoRoot, "tmp", "SampleVideo1mb.mp4");
+        var thumbnailFilePath = Path.Combine(repoRoot, "tmp", "sampleThumbnail.jpg");
         login.Login();
         _driver.Navigate().GoToUrl("http://145.220.74.148:3000/upload");
         _driver.FindElement(UploadPage.TitleInput).SendKeys($"Test-{new Guid().ToString()}");
         _driver.FindElement(UploadPage.DescriptionInput).SendKeys("Test Description, Lorem Ipsum Dolor Sit Amet Consectetur Adipiscing Elit Sed Do Eiusmod Tempor Incididunt Ut Labore Et Dolore Magna Aliqua Ut Enim Ad Minim Veniam Quis Nostrud Exercitation Ullamco Laboris Nisi Ut Aliquip Ex Ea Commodo Consequat");
         _driver.FindElement(UploadPage.CategoryInput).SendKeys("Music");
-        _driver.FindElement(UploadPage.SelectVideoButton).SendKeys(@"tmp\SampleVideo1mb.mp4");
-        _driver.FindElement(UploadPage.SelectThumbnail).SendKeys(@"tmp\sampleThumbnail.jpg");
+        _driver.FindElement(UploadPage.SelectVideoButton).SendKeys(videoFilePath);
+        _driver.FindElement(UploadPage.SelectThumbnail).SendKeys(thumbnailFilePath);
         _driver.FindElement(UploadPage.UploadButton).Click();
         Thread.Sleep(7000);
         var alert = _driver.SwitchTo().Alert();
@@ -31,6 +34,13 @@ public class UploadVideo
         Assert.Contains("Video uploaded successfully!", text);
         alert.Accept();
         _driver.SwitchTo().DefaultContent();
+    }
+    
+    
+    string GetRepositoryRoot()
+    {
+        // This could be an environment variable or a static path
+        return Environment.GetEnvironmentVariable("REPO_ROOT") ?? throw new InvalidOperationException("Repository root not set.");
     }
     
 }
